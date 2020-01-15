a871c565bb13d0ed70c03319b1a45a93dae83d8d
pauloswardblad <paulo@swardblads.se>
2020-01-16 00:38:27 +0100

    puts thomas and annas age

--- my_group.rb
+++ my_group.rb
@@ -3,3 +3,5 @@ characters_name = ["Person_1", "Person_2", "Person_3"]
 character_1 = {name: "Person_1", gender: 'male', age: 40}
 character_2 = {name: "Person_2", gender: 'female', age: 41}
 character_3 = {name: "Person_3", gender: 'male', age: 42}
+puts "Thomas is a 44 years old man"
+puts "Anna is a 38 years old woman"
