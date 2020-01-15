e08893327fee866a8b62e45c42178cb4f1431ed8
pauloswardblad <paulo@swardblads.se>
2020-01-16 00:26:55 +0100

    adding character names,gender,age

--- my_group.rb
+++ my_group.rb
@@ -1,14 +1,5 @@
-ar
-
-
-
-
-
-
-person = 
-{
-    name: "Person_1", "Person_2", "Person_3"
-    gender: 'male', 'female', 'male'
-    age: 40, 41, 42
-}
-end
+characters_name = Array.new
+characters_name = ["Person_1", "Person_2", "Person_3"]
+character_1 = {name: "Person_1", gender: 'male', age: 40}
+character_2 = {name: "Person_2", gender: 'female', age: 41}
+character_3 = {name: "Person_3", gender: 'male', age: 42}
