10ec93a04ff18fc3cb8d7f7ffa468dc293602a0f
pauloswardblad <paulo@swardblads.se>
2020-01-16 20:21:20 +0100

    refreshing

--- my_group.rb
+++ my_group.rb
@@ -3,6 +3,7 @@ characters_name = ["Person_1", "Person_2", "Person_3"]
 character_1 = {name: "Person_1", gender: 'male', age: 40}
 character_2 = {name: "Person_2", gender: 'female', age: 41}
 character_3 = {name: "Person_3", gender: 'male', age: 42}
-character_4 = {name: "Person_4"}
-puts ("is 44 years old man " + character_4)
-puts ("Anna is a 38 years old woman " + character_2)
+puts "is 44 years old man"
+puts "Anna is a 38 years old woman"
+
+end
