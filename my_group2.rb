bb8833f17f6a478d046c8b78b549c68350205513
pauloswardblad <paulo@swardblads.se>
2020-01-16 20:17:03 +0100

    refreshing

--- my_group2.rb
+++ my_group2.rb
@@ -1,10 +1,10 @@
-person_1 = {name:"Jocke", age:49, gender:'female'}
+person_1 = {name:"Jocke", age:25, gender:'male'}
 person_2 = {name:"Filippa", age:40, gender:'female'}
 person_3 = {name:"Robban", age:30, gender:'male'}
 
 group = [person_1, person_2, person_3]
 
 group.each do |person|
-    puts "#{person[:name]} is a #{person[:age]} and is a #{person[:gender]}"
+    puts "#{person[:name]} is a #{person[:age]} old person and is a #{person[:gender]}"
 
 end
