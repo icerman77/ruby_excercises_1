556535ea38d251cd3b71b54210ec96ec1b0c1e4a
pauloswardblad <paulo@swardblads.se>
2020-01-24 20:01:03 +0100

    adding a calculator and userform

---
+++ input-from-users.rb
@@ -0,0 +1,8 @@
+
+puts "Enter your name "
+name = gets.chomp()
+puts "Enter your age "
+age = gets.chomp()
+puts "Enter your emaladress"
+emailadress = gets
+puts ("Hello " + name + ", you are " + age + " and your email is " + emailadress)
